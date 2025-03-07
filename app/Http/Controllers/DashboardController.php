e6a23a30437a686a1baf209f4bb465088477b680
Cyrillus Dodi <cdoditri@gmail.com>
2025-03-07 08:27:42 +0700

    refactor DashboardController and routes for improved readability and consistency

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,122 +12,122 @@ use Filament\Notifications\Notification;
 
 class DashboardController extends Controller
 {
-  public function index()
-  {
+    public function index()
+    {
 
-    return view('dashboard/index');
-  }
+        return view('dashboard/index');
+    }
 
-  public function firstPrint()
-  {
+    public function firstPrint()
+    {
 
 
-    $functionary = Functionary::find(request()->input('functionary'));
+        $functionary = Functionary::find(request()->input('functionary'));
 
-    $biodata = Biodata::find(request()->input('biodata'));
-    $expDate = request()->input('exp_date');
+        $biodata = Biodata::find(request()->input('biodata'));
+        $expDate = request()->input('exp_date');
 
-    // check if already printed
-    if ($biodata->no_pendaftaran != null) {
-      Notification::make()
-        ->title('Sudah pernah dicetak')
-        ->warning()
-        ->send();
-      return redirect()->route('dasbboardShow', ['biodata' => $biodata->id]);
+        // check if already printed
+        if ($biodata->no_pendaftaran != null) {
+            Notification::make()
+                ->title('Sudah pernah dicetak')
+                ->warning()
+                ->send();
+            return redirect()->route('dashboardShow', ['biodata' => $biodata->id]);
+        }
+
+
+        DB::beginTransaction();
+
+        try {
+            $cetakTransaction = CetakTransaction::create([
+                'user_id' => auth()->user()->id,
+                'biodata_id' => $biodata->id,
+                'functionary_id' => $functionary->id,
+                'expired' => $expDate,
+            ]);
+
+            $noPendaftaran = $this->generateNoPendaftaran($biodata->nik);
+
+            $biodata->update(['no_pendaftaran' => $noPendaftaran]);
+
+            DB::commit();
+            return redirect()->route('dashboard.print', ['cetakTransaction' => $cetakTransaction]);
+        } catch (\Exception $e) {
+            // notification
+            Notification::make()
+                ->title('Gagal cetak')
+                ->body(env('APP_ENV') === 'production' ? $e->getCode() : $e->getMessage())
+                ->danger()
+                ->send();
+            DB::rollBack();
+
+            return redirect()->route('dashboardShow', ['biodata' => $biodata->id]);
+        }
+    }
+
+    // v2.1
+    public function print(CetakTransaction $cetakTransaction)
+    {
+        $functionary = Functionary::find($cetakTransaction->functionary_id);
+        $biodata = Biodata::find($cetakTransaction->biodata_id);
+        $expDate = $cetakTransaction->expired;
+
+        $pdf = Pdf::loadView('pdf.yellow-card', compact('functionary', 'biodata', 'expDate'));
+        Pdf::setOption(['isHtml5ParserEnabled' => true, 'isPhpEnabled' => true]);
+        $pdf->setPaper([0, 0, 297, 841.89], 'landscape');
+
+        // "a4" => array(0, 0, 595.28, 841.89),
+
+
+        // $pdf->setPaper([0.0, 0.0, 595.00, 420.50]);
+
+        $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
+
+        $pdf->getCanvas()->page_text(72, 18, "Header: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0, 0, 0));
+
+        return $pdf->stream('e-kaku_' . $biodata->no_pendaftaran . '.pdf');
+    }
+
+    private function generateNoPendaftaran($nik)
+    {
+
+        $prefix = substr($nik, 0, 4);
+        $suffix = date('dmY');
+
+        // cek apakah ada data pada bulan ini
+        $noUrut = CetakTransaction::whereYear('created_at', date('Y'))
+            ->whereMonth('created_at', date('m'))
+            ->count();
+
+        $noUrut = sprintf('%05d', $noUrut + 1);
+
+        return $prefix . $noUrut . $suffix;
     }
 
 
-    DB::beginTransaction();
+    public function show(Biodata $biodata)
+    {
 
-    try {
-      $cetakTransaction = CetakTransaction::create([
-        'user_id' => auth()->user()->id,
-        'biodata_id' => $biodata->id,
-        'functionary_id' => $functionary->id,
-        'expired' => $expDate,
-      ]);
+        // dd($biodata);
 
-      $noPendaftaran = $this->generateNoPendaftaran($biodata->nik);
+        $cetak = CetakTransaction::where('biodata_id', $biodata->id)->first();
 
-      $biodata->update(['no_pendaftaran' => $noPendaftaran]);
+        $functionaries = Functionary::where('is_active', 1)
+            ->orderBy('name', 'desc')
+            ->get();
 
-      DB::commit();
-      return redirect()->route('dashboard.print', ['cetakTransaction' => $cetakTransaction]);
-    } catch (\Exception $e) {
-      // notification
-      Notification::make()
-        ->title('Gagal cetak')
-        ->body(env('APP_ENV') === 'production' ? $e->getCode() : $e->getMessage())
-        ->danger()
-        ->send();
-      DB::rollBack();
-
-      return redirect()->route('dashboardShow', ['biodata' => $biodata->id]);
+        return view('dashboard.show', compact('biodata', 'cetak', 'functionaries'));
     }
-  }
 
-  // v2.1
-  public function print(CetakTransaction $cetakTransaction)
-  {
-    $functionary = Functionary::find($cetakTransaction->functionary_id);
-    $biodata = Biodata::find($cetakTransaction->biodata_id);
-    $expDate = $cetakTransaction->expired;
+    public function edit(Biodata $biodata)
+    {
+        return view('dashboard.edit', compact('biodata'));
+    }
 
-    $pdf = Pdf::loadView('pdf.yellow-card', compact('functionary', 'biodata', 'expDate'));
-    Pdf::setOption(['isHtml5ParserEnabled' => true, 'isPhpEnabled' => true]);
-    $pdf->setPaper([0, 0, 297, 841.89], 'landscape');
+    public function create()
+    {
 
-    // "a4" => array(0, 0, 595.28, 841.89),
-
-
-    // $pdf->setPaper([0.0, 0.0, 595.00, 420.50]);
-
-    $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-
-    $pdf->getCanvas()->page_text(72, 18, "Header: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0, 0, 0));
-
-    return $pdf->stream('e-kaku_' . $biodata->no_pendaftaran . '.pdf');
-  }
-
-  private function generateNoPendaftaran($nik)
-  {
-
-    $prefix = substr($nik, 0, 4);
-    $suffix = date('dmY');
-
-    // cek apakah ada data pada bulan ini
-    $noUrut = CetakTransaction::whereYear('created_at', date('Y'))
-      ->whereMonth('created_at', date('m'))
-      ->count();
-
-    $noUrut = sprintf('%05d', $noUrut + 1);
-
-    return $prefix . $noUrut . $suffix;
-  }
-
-
-  public function show(Biodata $biodata)
-  {
-
-    // dd($biodata);
-
-    $cetak = CetakTransaction::where('biodata_id', $biodata->id)->first();
-
-    $functionaries = Functionary::where('is_active', 1)
-      ->orderBy('name', 'desc')
-      ->get();
-
-    return view('dashboard.show', compact('biodata', 'cetak', 'functionaries'));
-  }
-
-  public function edit(Biodata $biodata)
-  {
-    return view('dashboard.edit', compact('biodata'));
-  }
-
-  public function create()
-  {
-
-    return view('dashboard.create');
-  }
+        return view('dashboard.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,42 +20,45 @@ use App\Http\Controllers\PengaturanController;
 */
 
 Route::get('/', function () {
-  return view('welcome');
+    return view('welcome');
 })->name('home');
 
 // Route::get('/home', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
-  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-  Route::get('/biodata', [BiodataController::class, 'index'])->name('biodata.index');
-  // Route::get('/biodata/show', [BiodataController::class, 'show'])->name('biodata.show');
-  // Route::get('/biodata/create', [BiodataController::class, 'create'])->name('biodata.create');
-  // Route::post('/biodata/create', [BiodataController::class, 'store']);
-  // Route::get('/biodata/edit/{biodata}', [BiodataController::class, 'edit'])->name('biodataEdit');
+    Route::get('/biodata', [BiodataController::class, 'index'])->name('biodata.index');
+    // Route::get('/biodata/show', [BiodataController::class, 'show'])->name('biodata.show');
+    // Route::get('/biodata/create', [BiodataController::class, 'create'])->name('biodata.create');
+    // Route::post('/biodata/create', [BiodataController::class, 'store']);
+    // Route::get('/biodata/edit/{biodata}', [BiodataController::class, 'edit'])->name('biodataEdit');
 
-  // delete berkas
-  Route::delete('/biodata/delete/berkas', [BiodataController::class, 'destroyBerkas'])->name('biodataDestroyBerkas');
+    // delete berkas
+    Route::delete('/biodata/delete/berkas', [BiodataController::class, 'destroyBerkas'])->name('biodataDestroyBerkas');
 
-  Route::middleware(['admin'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-    Route::get('/dashboard/first-print/', [DashboardController::class, 'firstPrint'])->name('dashboard.firstPrint');
-    Route::get('/dashboard/print/{cetakTransaction}', [DashboardController::class, 'print'])->name('dashboard.print');
-    // Route::get('/dashboard/print-view/{cetak_trans}', [DashboardController::class, 'printView'])->name('dashboardPrintView');
-    Route::get('/dashboard/show/{biodata}', [DashboardController::class, 'show'])->name('dashboardShow');
-    Route::get('/dashboard/edit/{biodata}', [DashboardController::class, 'edit'])->name('dashboardEdit');
-    // Route::get('/dashboard/create', [DashboardController::class, 'create'])->name('dashboardCreate');
-    Route::get('/dashboard/create', [DashboardController::class, 'create'])->name('dashboardCreate');
+    Route::middleware(['admin'])->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+        Route::get('/dashboard/first-print/', [DashboardController::class, 'firstPrint'])->name('dashboard.firstPrint');
+        Route::get('/dashboard/print/{cetakTransaction}', [DashboardController::class, 'print'])->name('dashboard.print');
+        // Route::get('/dashboard/print-view/{cetak_trans}', [DashboardController::class, 'printView'])->name('dashboardPrintView');
+        Route::get('/dashboard/show/{biodata}', [DashboardController::class, 'show'])
+            ->name('dashboardShow');
+        Route::get('/dashboard/edit/{biodata}', [DashboardController::class, 'edit'])
+            ->name('dashboardEdit');
+        // Route::get('/dashboard/create', [DashboardController::class, 'create'])->name('dashboardCreate');
+        Route::get('/dashboard/create', [DashboardController::class, 'create'])
+            ->name('dashboardCreate');
 
-    Route::get('/report', [ReportController::class, 'index'])->name('report.index');
-    Route::get('/report/export/{bulan}/{tahun}', [ReportController::class, 'export'])->name('report.export');
+        Route::get('/report', [ReportController::class, 'index'])->name('report.index');
+        Route::get('/report/export/{bulan}/{tahun}', [ReportController::class, 'export'])->name('report.export');
 
-    Route::get('/pengaturan', [PengaturanController::class, 'index'])->name('pengaturan.index');
-    Route::get('/pengaturan/pejabat', [PengaturanController::class, 'pejabatIndex'])->name('pengaturan.pejabat');
-    Route::get('/pengaturan/pejabat/create', [PengaturanController::class, 'pejabatCreate'])->name('pengaturan.pejabat.create');
-  });
+        Route::get('/pengaturan', [PengaturanController::class, 'index'])->name('pengaturan.index');
+        Route::get('/pengaturan/pejabat', [PengaturanController::class, 'pejabatIndex'])->name('pengaturan.pejabat');
+        Route::get('/pengaturan/pejabat/create', [PengaturanController::class, 'pejabatCreate'])->name('pengaturan.pejabat.create');
+    });
 });
 // Route::get('/test', [BiodataController::class, 'generateNoPendaftaran']);
 
